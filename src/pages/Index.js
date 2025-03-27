33f3fd54caedaf2db5eefc5c24162c950d6cd270
sheppardjm <jsheppard@neucadia.com>
2025-03-27 12:08:08 -0400

    Rename and combine header

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,39 +1,37 @@
-import React, { useEffect } from 'react';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
-import IndexSectionNavigations1 from '../components/navigations/IndexSectionNavigations1';
-import IndexSectionHeaders2 from '../components/headers/IndexSectionHeaders2';
-import IndexSectionFeatures3 from '../components/features/IndexSectionFeatures3';
-import IndexSectionCallToAction4 from '../components/call-to-action/IndexSectionCallToAction4';
-import IndexSectionFeatures5 from '../components/features/IndexSectionFeatures5';
-import IndexSectionCallToAction6 from '../components/call-to-action/IndexSectionCallToAction6';
-import IndexSection__structures25 from '../components/__structures/IndexSection__structures25';
-import IndexSectionHowItWorks8 from '../components/how-it-works/IndexSectionHowItWorks8';
-import IndexSection__structures33 from '../components/__structures/IndexSection__structures33';
-import IndexSectionCallToAction10 from '../components/call-to-action/IndexSectionCallToAction10';
-import IndexSectionCallToAction11 from '../components/call-to-action/IndexSectionCallToAction11';
-import IndexSectionHowItWorks12 from '../components/how-it-works/IndexSectionHowItWorks12';
-import IndexSectionPricing13 from '../components/pricing/IndexSectionPricing13';
-import IndexSection__structures29 from '../components/__structures/IndexSection__structures29';
-import IndexSection__structures28 from '../components/__structures/IndexSection__structures28';
-import IndexSectionPortfolio14 from '../components/portfolio/IndexSectionPortfolio14';
-import IndexSection__structures30 from '../components/__structures/IndexSection__structures30';
-import IndexSectionTeam15 from '../components/team/IndexSectionTeam15';
-import IndexSection__structures31 from '../components/__structures/IndexSection__structures31';
-import IndexSection__structures32 from '../components/__structures/IndexSection__structures32';
-import IndexSectionPortfolio26 from '../components/portfolio/IndexSectionPortfolio26';
-import IndexSectionApplication16 from '../components/application/IndexSectionApplication16';
-import IndexSectionTeam17 from '../components/team/IndexSectionTeam17';
-import IndexSectionTeam18 from '../components/team/IndexSectionTeam18';
-import IndexSectionCallToAction19 from '../components/call-to-action/IndexSectionCallToAction19';
-import IndexSectionSignIn20 from '../components/sign-in/IndexSectionSignIn20';
-import IndexSectionCallToAction21 from '../components/call-to-action/IndexSectionCallToAction21';
-import IndexSectionPricing23 from '../components/pricing/IndexSectionPricing23';
-import IndexSectionHowItWorks34 from '../components/how-it-works/IndexSectionHowItWorks34';
-import IndexSectionBlog27 from '../components/blog/IndexSectionBlog27';
-import IndexSectionFaq24 from '../components/faq/IndexSectionFaq24';
+import React, { useEffect } from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import Header from "../components/headers/header";
+import IndexSectionCallToAction4 from "../components/call-to-action/IndexSectionCallToAction4";
+import IndexSectionFeatures5 from "../components/features/IndexSectionFeatures5";
+import IndexSectionCallToAction6 from "../components/call-to-action/IndexSectionCallToAction6";
+import IndexSection__structures25 from "../components/__structures/IndexSection__structures25";
+import IndexSectionHowItWorks8 from "../components/how-it-works/IndexSectionHowItWorks8";
+import IndexSection__structures33 from "../components/__structures/IndexSection__structures33";
+import IndexSectionCallToAction10 from "../components/call-to-action/IndexSectionCallToAction10";
+import IndexSectionCallToAction11 from "../components/call-to-action/IndexSectionCallToAction11";
+import IndexSectionHowItWorks12 from "../components/how-it-works/IndexSectionHowItWorks12";
+import IndexSectionPricing13 from "../components/pricing/IndexSectionPricing13";
+import IndexSection__structures29 from "../components/__structures/IndexSection__structures29";
+import IndexSection__structures28 from "../components/__structures/IndexSection__structures28";
+import IndexSectionPortfolio14 from "../components/portfolio/IndexSectionPortfolio14";
+import IndexSection__structures30 from "../components/__structures/IndexSection__structures30";
+import IndexSectionTeam15 from "../components/team/IndexSectionTeam15";
+import IndexSection__structures31 from "../components/__structures/IndexSection__structures31";
+import IndexSection__structures32 from "../components/__structures/IndexSection__structures32";
+import IndexSectionPortfolio26 from "../components/portfolio/IndexSectionPortfolio26";
+import IndexSectionApplication16 from "../components/application/IndexSectionApplication16";
+import IndexSectionTeam17 from "../components/team/IndexSectionTeam17";
+import IndexSectionTeam18 from "../components/team/IndexSectionTeam18";
+import IndexSectionCallToAction19 from "../components/call-to-action/IndexSectionCallToAction19";
+import IndexSectionSignIn20 from "../components/sign-in/IndexSectionSignIn20";
+import IndexSectionCallToAction21 from "../components/call-to-action/IndexSectionCallToAction21";
+import IndexSectionPricing23 from "../components/pricing/IndexSectionPricing23";
+import IndexSectionHowItWorks34 from "../components/how-it-works/IndexSectionHowItWorks34";
+import IndexSectionBlog27 from "../components/blog/IndexSectionBlog27";
+import IndexSectionFaq24 from "../components/faq/IndexSectionFaq24";
 
 const meta = {
-  title: '',
+  title: "",
   meta: [],
   link: [],
   style: [],
@@ -46,7 +44,7 @@ export default function Index() {
     let classes = document.body.classList;
     document.body.classList.remove(...classes);
     document.body.classList.add(
-      ...'antialiased font-body bg-body text-body .body'.split(' ')
+      ..."antialiased font-body bg-body text-body .body".split(" ")
     );
   });
 
@@ -55,9 +53,7 @@ export default function Index() {
       <HelmetProvider>
         <Helmet {...meta}></Helmet>
       </HelmetProvider>
-      <IndexSectionNavigations1 />
-      <IndexSectionHeaders2 />
-      <IndexSectionFeatures3 />
+      <Header />
       <IndexSectionCallToAction4 />
       <IndexSectionFeatures5 />
       <IndexSectionCallToAction6 />
@@ -89,4 +85,3 @@ export default function Index() {
     </React.Fragment>
   );
 }
-

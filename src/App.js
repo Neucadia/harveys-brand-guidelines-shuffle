f9c7bda0ebe0c51e7f2c9880c4720f37c95dd867
sheppardjm <jsheppard@neucadia.com>
2025-03-27 13:48:52 -0400

    Remove colors page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React from 'react';
-import {
-  Routes,
-  Route
-} from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import IndexPage from './pages/Index.js';
-import ColorsPage from './pages/Colors.js';
+import IndexPage from "./pages/Index.js";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<IndexPage />} />
-         
-      <Route path="/colors" element={<ColorsPage />} />
     </Routes>
   );
 }

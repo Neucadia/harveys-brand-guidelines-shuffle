b1844086ef0a3e0fabcacb7deb0880fa7f13fa97
sheppardjm <jsheppard@neucadia.com>
2025-04-01 07:46:36 -0400

    Remove dup section

--- src/components/photography.js
+++ src/components/photography.js
@@ -163,65 +163,6 @@ export default function IndexSection__structures29() {
             </div>
           </div>
         </section>
-        <section className="bg-yellow-50 pt-20">
-          <div className="container mx-auto px-4">
-            <h1 className="font-heading text-center text-4xl lg:text-5xl font-bold max-w-2xl lg:max-w-5xl mx-auto mb-8 uppercase tracking-widest">
-              retail store photography
-            </h1>
-            <p className="text-gray-600 text-center max-w-xl mx-auto mb-12">
-              For our retail store photography, we use a mix of up-close product
-              shots, merchandise displays, and lifestyle photos to bring our
-              brand to life.
-            </p>
-          </div>
-          <div className="container mx-auto px-4">
-            <div className="flex flex-wrap -m-4">
-              <div className="w-full lg:w-1/2 p-4">
-                <div className="relative">
-                  <img
-                    className="w-full h-72 object-cover"
-                    src="https://static.shuffle.dev/uploads/files/ee/ee137d2a3df0a9e3dc5d8722b3ba155636636edc/Harveys22-0025-4-copy.jpg"
-                    alt
-                  />
-                </div>
-              </div>
-              <div className="w-full lg:w-1/2 p-4">
-                <div className="relative">
-                  <img
-                    className="w-full h-72 object-cover"
-                    src="https://static.shuffle.dev/uploads/files/ee/ee137d2a3df0a9e3dc5d8722b3ba155636636edc/Harveys22-0018-1-copy-jpg-1742387935302.webp"
-                    alt
-                  />
-                </div>
-              </div>
-              <div className="w-full lg:w-1/3 p-4">
-                <div className="relative">
-                  <img
-                    className="w-full h-72 object-cover"
-                    src="https://static.shuffle.dev/uploads/files/ee/ee137d2a3df0a9e3dc5d8722b3ba155636636edc/Harveys22-0009-6-copy.jpg"
-                    alt
-                  />
-                </div>
-              </div>
-              <div className="w-full lg:w-1/3 p-4">
-                <img
-                  className="w-full h-72 object-cover"
-                  src="https://static.shuffle.dev/uploads/files/ee/ee137d2a3df0a9e3dc5d8722b3ba155636636edc/Harveys22-0027-2-copy.jpg"
-                  alt
-                />
-              </div>
-              <div className="w-full lg:w-1/3 p-4">
-                <div className="relative">
-                  <img
-                    className="w-full h-72 object-cover"
-                    src="https://static.shuffle.dev/uploads/files/ee/ee137d2a3df0a9e3dc5d8722b3ba155636636edc/Harveys22-0019-3-copy.jpg"
-                    alt
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
         <section className="py-20 relative bg-yellow-50">
           <div
             className="w-1/2 mt-20 absolute hidden lg:block top-0 left-0 -mr-32"

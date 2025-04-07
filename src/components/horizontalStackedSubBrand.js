f7e86379ba062140319121495a933d5fc4de0a2c
sheppardjm <jsheppard@neucadia.com>
2025-04-07 14:52:52 -0400

    Fix horzital stacked sub switcher

--- src/components/horizontalStackedSubBrand.js
+++ src/components/horizontalStackedSubBrand.js
@@ -131,9 +131,9 @@ const HorizontalStackedSubBrand = () => {
           </a>
         </div>
       </div>
-      <div className="flex flex-wrap flex-row items-center -m-4">
-        <div className="w-full md:w-1/5 lg:w-1/5 p-4 mb-6 lg:mb-0 text-center">
-          <div className="inline-block mx-auto mb-6 relative">
+      <div className="flex flex-wrap flex-row items-center -m-4 justify-center">
+        <div className="w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+          <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-black rounded-full m-1" />
             <span className="relative">
               <svg
@@ -169,8 +169,8 @@ const HorizontalStackedSubBrand = () => {
             </a>
           </div>
         </div>
-        <div className="w-full md:w-1/5 lg:w-1/5 p-4 mb-6 lg:mb-0 text-center">
-          <div className="inline-block mx-auto mb-6 relative">
+        <div className="w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+          <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-green-700 rounded-full m-1" />
             <span className="relative">
               <svg
@@ -206,8 +206,8 @@ const HorizontalStackedSubBrand = () => {
             </a>
           </div>
         </div>
-        <div className="w-full md:w-1/5 lg:w-1/5 p-4 mb-6 lg:mb-0 text-center">
-          <div className="inline-block mx-auto mb-6 relative">
+        <div className="w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+          <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-orange-500 rounded-full m-1" />
             <span className="relative">
               <svg
@@ -243,8 +243,8 @@ const HorizontalStackedSubBrand = () => {
             </a>
           </div>
         </div>
-        <div className="w-full md:w-1/5 lg:w-1/5 p-4 text-center">
-          <div className="inline-block mx-auto mb-6 relative">
+        <div className="w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+          <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-yellow-50 rounded-full m-1" />
             <span className="relative">
               <svg
@@ -280,8 +280,8 @@ const HorizontalStackedSubBrand = () => {
             </a>
           </div>
         </div>
-        <div className="w-full md:w-1/5 lg:w-1/5 p-4 text-center">
-          <div className="inline-block mx-auto mb-6 relative">
+        <div className="w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+          <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-white rounded-full m-1" />
             <span className="relative">
               <svg

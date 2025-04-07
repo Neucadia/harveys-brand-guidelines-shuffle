fda327069438df8028083acd5fd230f0c33e5cbc
sheppardjm <jsheppard@neucadia.com>
2025-04-07 14:44:47 -0400

    Fix horizontal lockup switcher negative stacked padding

--- src/components/negativeStackedEgg.js
+++ src/components/negativeStackedEgg.js
@@ -54,7 +54,7 @@ const NegativeStackedEgg = () => {
         </div>
       </div>
       <div className="flex flex-wrap flex-row items-center -m-4 justify-center">
-        <div className="w-1/5 p-4 mb-6 lg:mb-0 text-center">
+        <div className="w-1/5 p-0 md:p-0 md:p-4 mb-6 lg:mb-0 text-center">
           <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-black rounded-full m-1" />
             <span className="relative">
@@ -95,7 +95,7 @@ const NegativeStackedEgg = () => {
             </a>
           </div>
         </div>
-        <div className="w-1/5 p-4 mb-6 lg:mb-0 text-center">
+        <div className="w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
           <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-green-700 rounded-full m-1" />
             <span className="relative">
@@ -136,7 +136,7 @@ const NegativeStackedEgg = () => {
             </a>
           </div>
         </div>
-        <div className="w-1/5 p-4 mb-6 lg:mb-0 text-center">
+        <div className="w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
           <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-orange-500 rounded-full m-1" />
             <span className="relative">
@@ -218,7 +218,7 @@ const NegativeStackedEgg = () => {
             </a>
           </div>
         </div>
-        <div className="w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+        <div className="w-1/5 p-0 md:p-0 md:p-4 mb-6 lg:mb-0 text-center">
           <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-white rounded-full m-1" />
             <span className="relative">

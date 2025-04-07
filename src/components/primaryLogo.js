ebc16cb2ea92a6706c3f21fab324383e313b8f55
sheppardjm <jsheppard@neucadia.com>
2025-04-07 14:05:53 -0400

    Fix primary logo color selector

--- src/components/primaryLogo.js
+++ src/components/primaryLogo.js
@@ -264,9 +264,9 @@ const PrimaryLogo = () => {
           </a>
         </div>
       </div>
-      <div className="flex flex-wrap flex-row items-center -m-4">
-        <div className="w-full md:w-1/5 lg:w-1/5 p-4 mb-6 lg:mb-0 text-center">
-          <div className="inline-block mx-auto mb-6 relative">
+      <div className="flex flex-wrap flex-row items-center -m-4 justify-center">
+        <div className="md:w-1/5 lg:w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+          <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-black rounded-full m-1" />
             <span className="relative">
               <svg
@@ -302,8 +302,8 @@ const PrimaryLogo = () => {
             </a>
           </div>
         </div>
-        <div className="w-full md:w-1/5 lg:w-1/5 p-4 mb-6 lg:mb-0 text-center">
-          <div className="inline-block mx-auto mb-6 relative">
+        <div className="md:w-1/5 lg:w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+          <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-green-700 rounded-full m-1" />
             <span className="relative">
               <svg
@@ -339,8 +339,8 @@ const PrimaryLogo = () => {
             </a>
           </div>
         </div>
-        <div className="w-full md:w-1/5 lg:w-1/5 p-4 mb-6 lg:mb-0 text-center">
-          <div className="inline-block mx-auto mb-6 relative">
+        <div className="md:w-1/5 lg:w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+          <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-orange-500 rounded-full m-1" />
             <span className="relative">
               <svg
@@ -376,8 +376,8 @@ const PrimaryLogo = () => {
             </a>
           </div>
         </div>
-        <div className="w-full md:w-1/5 lg:w-1/5 p-4 text-center">
-          <div className="inline-block mx-auto mb-6 relative">
+        <div className="md:w-1/5 lg:w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+          <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-yellow-50 rounded-full m-1" />
             <span className="relative">
               <svg
@@ -413,8 +413,8 @@ const PrimaryLogo = () => {
             </a>
           </div>
         </div>
-        <div className="w-full md:w-1/5 lg:w-1/5 p-4 text-center">
-          <div className="inline-block mx-auto mb-6 relative">
+        <div className="md:w-1/5 lg:w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+          <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-white rounded-full m-1" />
             <span className="relative">
               <svg

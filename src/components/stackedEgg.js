bc2d8aeef66abac99b494a44fd927810d226cbe7
sheppardjm <jsheppard@neucadia.com>
2025-04-07 14:38:09 -0400

    Fix stacked egg color switcher

--- src/components/stackedEgg.js
+++ src/components/stackedEgg.js
@@ -50,9 +50,9 @@ const StackedEgg = () => {
           </a>
         </div>
       </div>
-      <div className="flex flex-wrap flex-row items-center -m-4">
-        <div className="w-full md:w-1/5 lg:w-1/5 p-4 mb-6 lg:mb-0 text-center">
-          <div className="inline-block mx-auto mb-6 relative">
+      <div className="flex flex-wrap flex-row items-center -m-4 justify-center">
+        <div className="w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+          <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-black rounded-full m-1" />
             <span className="relative">
               <svg
@@ -88,8 +88,8 @@ const StackedEgg = () => {
             </a>
           </div>
         </div>
-        <div className="w-full md:w-1/5 lg:w-1/5 p-4 mb-6 lg:mb-0 text-center">
-          <div className="inline-block mx-auto mb-6 relative">
+        <div className="w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+          <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-green-700 rounded-full m-1" />
             <span className="relative">
               <svg
@@ -125,8 +125,8 @@ const StackedEgg = () => {
             </a>
           </div>
         </div>
-        <div className="w-full md:w-1/5 lg:w-1/5 p-4 mb-6 lg:mb-0 text-center">
-          <div className="inline-block mx-auto mb-6 relative">
+        <div className="w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+          <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-orange-500 rounded-full m-1" />
             <span className="relative">
               <svg
@@ -162,8 +162,8 @@ const StackedEgg = () => {
             </a>
           </div>
         </div>
-        <div className="w-full md:w-1/5 lg:w-1/5 p-4 text-center">
-          <div className="inline-block mx-auto mb-6 relative">
+        <div className="w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+          <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-yellow-50 rounded-full m-1" />
             <span className="relative">
               <svg
@@ -199,8 +199,8 @@ const StackedEgg = () => {
             </a>
           </div>
         </div>
-        <div className="w-full md:w-1/5 lg:w-1/5 p-4 text-center">
-          <div className="inline-block mx-auto mb-6 relative">
+        <div className="w-1/5 p-0 md:p-4 mb-6 lg:mb-0 text-center">
+          <div className="scale-50 md:scale-100 inline-block mx-auto mb-6 relative">
             <div className="absolute inset-0 flex justify-center items-center bg-white rounded-full m-1" />
             <span className="relative">
               <svg

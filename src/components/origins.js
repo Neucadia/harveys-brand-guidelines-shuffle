e3148e5fa775305a5883f10e9ec120cf25568da3
sheppardjm <jsheppard@neucadia.com>
2025-04-08 10:55:21 -0400

    Add other Kerry origin stuff

--- src/components/origins.js
+++ src/components/origins.js
@@ -27,23 +27,152 @@ export default function Origins() {
                 alt
               />
             </div>
-            <div className="py-20 relative" style={{ height: 512 }}>
+          </div>
+        </section>
+        <section className="py-20 bg-yellow-50">
+          <div className="container mx-auto px-4">
+            <div className="max-w-2xl mx-auto text-center mb-12">
+              <h1 className="font-heading text-4xl font-bold mb-6 text-center lg:text-5xl max-w-2xl lg:max-w-5xl mx-auto uppercase tracking-widest">
+                upholding tradition
+              </h1>
+              <div className="flex items-center justify-center">
+                <div className="mr-4"></div>
+                <div className="text-left">
+                  <p className="text-gray-600 text-xl text-center">
+                    Today, Harvey’s remains a family-owned, community-driven
+                    business that stands by the same principles it was founded
+                    on —Quality, integrity, and service—values that remain at
+                    the core of everything we do.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="mb-12 relative">
               <img
-                className="relative w-full object-cover h-full"
-                src="https://static.shuffle.dev/uploads/files/ee/ee137d2a3df0a9e3dc5d8722b3ba155636636edc/history-2-1.jpg"
+                className="w-full object-cover font-bold mb-4 font-heading font-heading text-4xl text-center lg:text-5xl max-w-2xl lg:max-w-5xl mx-auto uppercase tracking-widest"
+                src="https://static.shuffle.dev/uploads/files/61/61d552f6a2f836b26c9c27fbebc5b63130cdd6f2/history-2-1.jpg"
                 alt
               />
             </div>
-            <h6 className="font-bold mb-4 font-heading uppercase tracking-widest text-4xl text-center">
-              upholding tradition
-            </h6>
-            <p className="text-center mx-auto max-w-lg text-gray-600 text-xl">
-              Today, Harvey’s remains a family-owned, community-driven business
-              that stands by the same principles it was founded on —Quality,
-              integrity, and service—values that remain at the core of
-              everything we do.
-            </p>
           </div>
+          <div className="container mx-auto px-4"></div>
+        </section>
+        <section className="py-20 bg-yellow-50">
+          <div className="container mx-auto px-4">
+            <div className="flex flex-wrap -m-4">
+              <div className="w-full lg:w-1/3 p-4">
+                <ul>
+                  <li className="mb-6">
+                    <a
+                      className="font-bold font-heading text-4xl lg:text-5xl max-w-2xl lg:max-w-5xl mx-auto uppercase tracking-widest text-green-500 text-green-500"
+                      href="#"
+                    >
+                      honoring the past, forging the future
+                    </a>
+                  </li>
+                  <li className="mb-6 text-black">
+                    <a className="text-gray-600 text-xl" href="#">
+                      Over the decades, we’ve evolved to meet the needs of our
+                      customers, growing from a small feed operation into a
+                      multi-division company offering Feed, Seed, Commodities,
+                      Garden, and Retail services.
+                    </a>
+                  </li>
+                  <li className="mb-6"></li>
+                  <li></li>
+                </ul>
+                <img
+                  className="w-full object-cover"
+                  style={{ height: 445 }}
+                  src="https://static.shuffle.dev/uploads/files/61/61d552f6a2f836b26c9c27fbebc5b63130cdd6f2/486352657-1203409291787301-1729855104243629523-n.jpg"
+                  alt
+                />
+              </div>
+              <div className="w-full lg:w-2/3 p-4">
+                <div className="flex flex-wrap -m-4">
+                  <div className="w-full lg:w-1/3 p-4"></div>
+                  <div className="w-full lg:w-2/3 p-4">
+                    <img
+                      className="w-full object-cover"
+                      style={{ height: 445 }}
+                      src="https://static.shuffle.dev/uploads/files/61/61d552f6a2f836b26c9c27fbebc5b63130cdd6f2/Mill-Exterior.jpg"
+                      alt
+                    />
+                    <div className="relative">
+                      <img
+                        className="absolute hidden lg:block top-0 right-0 -mr-40"
+                        src="cronos-assets/elements/warning-group-square.svg"
+                        alt
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-2/3 p-4">
+                    <div className="relative">
+                      <img
+                        className="w-full object-cover"
+                        style={{ height: 445 }}
+                        src="https://static.shuffle.dev/uploads/files/61/61d552f6a2f836b26c9c27fbebc5b63130cdd6f2/Harveys22-0020-5-copy.jpg"
+                        alt
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-1/3 p-4">
+                    <div className="relative">
+                      <img
+                        className="absolute hidden lg:block bottom-0 right-0 w-56 -mr-28 z-10"
+                        src="cronos-assets/elements/light-warning-dark-green.svg"
+                        alt
+                      />
+                      <img
+                        className="w-full object-cover"
+                        style={{ height: 445 }}
+                        src="https://static.shuffle.dev/uploads/files/61/61d552f6a2f836b26c9c27fbebc5b63130cdd6f2/foodplot-quinn-2-7.jpg"
+                        alt
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="py-20 bg-yellow-50">
+          <div className="container mx-auto px-4">
+            <div className="max-w-2xl mx-auto text-center mb-12">
+              <h1 className="font-heading text-4xl font-bold mb-6 text-center lg:text-5xl max-w-2xl lg:max-w-5xl mx-auto uppercase tracking-widest">
+                Standing By farmers from the start
+              </h1>
+              <div className="flex items-center justify-center">
+                <div className="mr-4"></div>
+                <div className="text-left">
+                  <p className="text-gray-600 text-xl text-center">
+                    For generations, we’ve stood by our customers with
+                    reliability, expertise, and an unwavering dedication to
+                    their success. Because at Harvey’s, agriculture isn’t just
+                    business—it’s a way of life.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="mb-12 relative">
+              <img
+                className="absolute hidden lg:block top-0 right-0 w-40 h-40 z-10"
+                src="cronos-assets/elements/warning-light-small.svg"
+                alt
+              />
+              <img
+                className="absolute hidden lg:block bottom-0 left-0 w-40 h-40 z-10"
+                src="cronos-assets/elements/white-green-dark.svg"
+                alt
+              />
+              <img
+                className="w-full object-cover"
+                src="https://static.shuffle.dev/uploads/files/61/61d552f6a2f836b26c9c27fbebc5b63130cdd6f2/Harveys22-0006-copy.jpg"
+                alt
+              />
+            </div>
+          </div>
+          <div className="container mx-auto px-4"></div>
         </section>
       </>
     </React.Fragment>

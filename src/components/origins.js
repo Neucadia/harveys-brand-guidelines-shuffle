16adb9b170b62bafe02a3ecf82c52da6739e8c67
sheppardjm <jsheppard@neucadia.com>
2025-04-09 10:36:09 -0400

    Fix origins padding and cow img

--- src/components/origins.js
+++ src/components/origins.js
@@ -4,7 +4,7 @@ export default function Origins() {
   return (
     <React.Fragment>
       <>
-        <section className="py-20 bg-yellow-50">
+        <section className="pt-20 bg-yellow-50">
           <div className="container mx-auto px-4">
             <div className="flex justify-center mb-6">
               <div className="bg-yellow-100 py-1 px-2 font-bold inline-block">
@@ -29,7 +29,7 @@ export default function Origins() {
             </div>
           </div>
         </section>
-        <section className="py-20 bg-yellow-50">
+        <section className="pt-20 pb-10 bg-yellow-50">
           <div className="container mx-auto px-4">
             <div className="max-w-2xl mx-auto text-center mb-12">
               <h1 className="font-heading text-4xl font-bold mb-6 text-center lg:text-5xl max-w-2xl lg:max-w-5xl mx-auto uppercase tracking-widest">
@@ -57,18 +57,18 @@ export default function Origins() {
           </div>
           <div className="container mx-auto px-4"></div>
         </section>
-        <section className="py-20 bg-yellow-50">
+        <section className="pt-10 bg-yellow-50">
           <div className="container mx-auto px-4">
             <div className="flex flex-wrap -m-4">
               <div className="w-full lg:w-1/3 p-4">
                 <ul>
                   <li className="mb-6">
-                    <a
+                    <h2
                       className="font-bold font-heading text-4xl lg:text-5xl max-w-2xl lg:max-w-5xl mx-auto uppercase tracking-widest text-green-500 text-green-500"
                       href="#"
                     >
                       honoring the past, forging the future
-                    </a>
+                    </h2>
                   </li>
                   <li className="mb-6 text-black">
                     <a className="text-gray-600 text-xl" href="#">
@@ -78,8 +78,6 @@ export default function Origins() {
                       Garden, and Retail services.
                     </a>
                   </li>
-                  <li className="mb-6"></li>
-                  <li></li>
                 </ul>
                 <img
                   className="w-full object-cover"
@@ -90,7 +88,12 @@ export default function Origins() {
               </div>
               <div className="w-full lg:w-2/3 p-4">
                 <div className="flex flex-wrap -m-4">
-                  <div className="w-full lg:w-1/3 p-4"></div>
+                  <div className="w-full lg:w-1/3 p-4">
+                    <img
+                      src="https://static.shuffle.dev/uploads/files/ee/ee137d2a3df0a9e3dc5d8722b3ba155636636edc/amber-kipp-Mm-D-kbvTUM-unsplash.jpg"
+                      class="h-full object-cover"
+                    />
+                  </div>
                   <div className="w-full lg:w-2/3 p-4">
                     <img
                       className="w-full object-cover"
@@ -98,13 +101,6 @@ export default function Origins() {
                       src="https://static.shuffle.dev/uploads/files/61/61d552f6a2f836b26c9c27fbebc5b63130cdd6f2/Mill-Exterior.jpg"
                       alt
                     />
-                    <div className="relative">
-                      <img
-                        className="absolute hidden lg:block top-0 right-0 -mr-40"
-                        src="cronos-assets/elements/warning-group-square.svg"
-                        alt
-                      />
-                    </div>
                   </div>
                   <div className="w-full lg:w-2/3 p-4">
                     <div className="relative">
@@ -118,11 +114,6 @@ export default function Origins() {
                   </div>
                   <div className="w-full lg:w-1/3 p-4">
                     <div className="relative">
-                      <img
-                        className="absolute hidden lg:block bottom-0 right-0 w-56 -mr-28 z-10"
-                        src="cronos-assets/elements/light-warning-dark-green.svg"
-                        alt
-                      />
                       <img
                         className="w-full object-cover"
                         style={{ height: 445 }}
